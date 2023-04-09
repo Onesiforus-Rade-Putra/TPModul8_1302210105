f778edf11617f19f1964cbf7bba28f65dc3bf1e8
Onesiforus-Rade-Putra <onesiforusradeputra@gmail.com>
2023-04-09 19:35:09 +0700

    menyelesaikan program dengan runtime configuration

--- TPModul8_1302210105/Program.cs
+++ TPModul8_1302210105/Program.cs
@@ -3,18 +3,18 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        covid CovidConfig = new covid();
-        Console.Write("Berapa suhu badan anda?" + covid.covid.satuan_suhu + ": ");
+        covid covid = new covid();
+        Console.Write("Berapa suhu badan anda?" + covid.CovidConfig.satuan_suhu + ": ");
         double suhu = double.Parse(Console.ReadLine());
         Console.Write("Berapa hari mengalami gejala deman? : ");
         int hariDemam = int.Parse(Console.ReadLine());
         if (CovidConfig.inputan(suhu, hariDemam))
         {
-            Console.WriteLine(covid.covid.pesan_diterima);
+            Console.WriteLine(covid.CovidConfig.pesan_diterima);
         }
         else
         {
-            Console.WriteLine(covid.covid.pesan_ditolak);
+            Console.WriteLine(covid.CovidConfig.pesan_ditolak);
         }
     }
 }
